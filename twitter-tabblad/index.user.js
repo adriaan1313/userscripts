34d468e775c8d2a40f1281f77649d522671ca556
Bunnygamers <adriaan1313@outlook.com>
2023-07-01 23:02:45 +0200

    Update index.user.js

--- twitter-tabblad/index.user.js
+++ twitter-tabblad/index.user.js
@@ -1,22 +1,21 @@
 // ==UserScript==
 // @name         Default following tab Twitter.
 // @namespace    https://github.com/adriaan1313/userscripts/tree/master/twitter-tabblad
-// @version      0.2
+// @version      0.4
 // @description  By default go to the "following" tab on the Twitter home page
 // @author       Bunnygamers
-// @match        https://twitter.com/home
-// @match        https://twitter.com/
+// @match        https://twitter.com/*
+// @match        http://twitter.com/*
 // @icon         https://abs.twimg.com/favicons/twitter-pip.2.ico
 // @grant        none
 // @installURL   https://cdn.jsdelivr.net/gh/adriaan1313/userscripts@master/twitter-tabblad/index.user.js
 // @updateURL    https://cdn.jsdelivr.net/gh/adriaan1313/userscripts@master/twitter-tabblad/index.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/adriaan1313/userscripts@master/twitter-tabblad/index.user.js
 // @homepageURL  https://github.com/adriaan1313/userscripts/tree/master/twitter-tabblad
+// @require      https://gist.githubusercontent.com/mjblay/18d34d861e981b7785e407c3b443b99b/raw/debc0e6d4d537ac228d1d71f44b1162979a5278c/waitForKeyElements.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    setTimeout(()=>{// should make this a proper way of detecting this at some point
-        document.querySelectorAll("a[role=tab]")[1].click();
-    },800);
+    waitForKeyElements("a[role=tab]",()=>{if(location.href.endsWith("twitter.com/")||location.href.endsWith("twitter.com")||location.href.endsWith("twitter.com/home")||location.href.endsWith("twitter.com/home/"))document.querySelectorAll("a[role=tab]")[1].click();});
 })();
